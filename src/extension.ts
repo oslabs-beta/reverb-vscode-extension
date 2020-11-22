92f49ce325ffc80b074a4bca511ad06bebbeb275
Sean <itsmesean@pm.me>
2020-11-21 23:51:30 -0800

    Comments $ moved output channel invocation
    Changes made:
    
    Co-authored-by: Christopher Johnson <cjbeats@gmail.com>
    Co-authored-by: Amir Marcel <amirmarcel@yahoo.com>
    Co-authored-by: Corey Van Splinter <cvanspl1@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -4,20 +4,33 @@ import { testEndpoint } from './utils/testEndpoint';
 import { configEndpoint } from './utils/configEndpoint';
 
 export async function activate(context: vscode.ExtensionContext) {
-  const { rootPath } = vscode.workspace;
-  await context.workspaceState.update(`obj`, undefined);
-  utils.init(rootPath);
+  // uncomment to wipe storage
+  // await context.workspaceState.update(`obj`, undefined);
+  console.log(`INIT STATE => ${await context.workspaceState.get(`obj`)}`);
 
+  // Init output window. Needs to be passed to functions that use it.
+  const outputWindow: vscode.OutputChannel = vscode.window.createOutputChannel(
+    'reVerb',
+  );
+  outputWindow.appendLine('---reVerb initialized---');
+  utils.init();
+
+  /**
+   * Takes config and makes axios request
+   * @param {ExtensionContext} context context provided by vscode during activation
+   * @param {vscode.OutputChannel} outputWindow reVerb output initialized in extension.ts
+   */
   const disposable1 = vscode.commands.registerCommand(
     'extension.testRoute',
     async () => {
-      testEndpoint(context);
+      testEndpoint(context, outputWindow);
     },
   );
 
   const disposable2 = vscode.commands.registerCommand(
     'extension.configRoute',
     async () => {
+      // only for testing. Just changes GET to PUT for validation. See file for info.
       configEndpoint(context);
     },
   );
