fc9e1dde4d8980ce22bb4fe96c72787081c145a7
Sean <itsmesean@pm.me>
2020-11-21 17:36:10 -0800

    Remove excess and unused components
    Changes made:
    
    Co-authored-by: Christopher Johnson <cjbeats@gmail.com>
    Co-authored-by: Amir Marcel <amirmarcel@yahoo.com>
    Co-authored-by: Corey Van Splinter <cvanspl1@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,28 @@
 import * as vscode from 'vscode';
 import * as utils from './utils/utils';
 import { testEndpoint } from './utils/testEndpoint';
-import { getRanges } from './parser/utils/ast';
-import * as fs from 'fs';
+import { configEndpoint } from './utils/configEndpoint';
 
-// runs on extension startup
 export async function activate(context: vscode.ExtensionContext) {
   const { rootPath } = vscode.workspace;
+  await context.workspaceState.update(`obj`, undefined);
   utils.init(rootPath);
-  // wipe storage on start for dev testing
-  await context.workspaceState.update('obj', undefined);
 
   const disposable1 = vscode.commands.registerCommand(
     'extension.testRoute',
     async () => {
       testEndpoint(context);
-      // const FILETEXT = fs.readFileSync(
-      //   'C:/Users/itsme/Documents/test-server-express/server4/src/routes/ApiRouter.ts',
-      //   'utf8',
-      // );
-      // const output = getRanges(FILETEXT);
-      // console.log(output);
     },
   );
 
-  context.subscriptions.push(disposable1);
+  const disposable2 = vscode.commands.registerCommand(
+    'extension.configRoute',
+    async () => {
+      configEndpoint(context);
+    },
+  );
+
+  context.subscriptions.push(disposable1, disposable2);
 }
 
 export function deactivate() {
