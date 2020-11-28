c230d6f39dd2191ea1d02e9d8891611a10a90cf7
Sean <itsmesean@pm.me>
2020-11-28 11:31:39 -0800

    move logic from main to header
    Changes made:
    
    Co-authored-by: Christopher Johnson <cjbeats@gmail.com>
    Co-authored-by: Amir Marcel <amirmarcel@yahoo.com>
    Co-authored-by: Corey Van Splinter <cvanspl1@gmail.com>

--- react-app/src/containers/Header.jsx
+++ react-app/src/containers/Header.jsx
@@ -1,17 +1,30 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { routes } from "../../redux/reducers/routesSlice";
 import Select from "../components/Select";
 
-function Header({ routesArr, axiosReq }) {
-  console.log(routesArr, "i");
+function Header() {
+  const data = useSelector(routes);
+  console.log("header.jsx => Header => routes:", data);
+
+  const routesArr = [];
+  if (data) {
+    Object.keys(data).forEach((key) => {
+      Object.keys(data[key]).forEach((subkey) => {
+        routesArr.push(
+          <option key={subkey} value={subkey}>
+            {subkey}
+          </option>,
+        );
+      });
+    });
+  }
+
   return (
     <div className="container__header">
       <div className="header">
         <div className="group__left">
-          <Select
-            axiosReq={axiosReq}
-            routesArr={routesArr}
-            group="group__left"
-          />
+          <Select routes={routesArr} />
         </div>
         <div className="group__right">
           <select name="route" className="select__endpoint">
--- react-app/src/containers/Main.jsx
+++ react-app/src/containers/Main.jsx
@@ -1,37 +1,12 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { routes } from "../../redux/reducers/routesSlice";
-import create from "../Interaction/InteractorApi";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Input from "./Input";
 
 function Main() {
-  const Interactor = create(vscode);
-  const routesObj = useSelector(routes);
-
-  function axiosReq(data) {
-    console.log(data.route);
-    // const { config } = routesObj[route.key][route.subkey][type];
-    // Interactor.axiosReq(config);
-  }
-
-  const routesArr = [];
-  Object.keys(routesObj).forEach((key) => {
-    Object.keys(routesObj[key]).forEach((subkey) => {
-      const vals = { key, subkey, types: Object.keys(routesObj[key][subkey]) };
-      console.log(vals);
-      routesArr.push(
-        <option key={subkey} value={vals}>
-          {subkey}
-        </option>,
-      );
-    });
-  });
-
   return (
     <div className="container__main">
-      <Header routesArr={routesArr} axiosReq={axiosReq} />
+      <Header />
       <Input />
       <Sidebar />
     </div>
