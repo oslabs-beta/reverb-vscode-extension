0c169f2d2f29b86004d430599efe612b3374fa4e
Sean <itsmesean@pm.me>
2020-11-28 11:30:36 -0800

    Different way of setting redux stores
    Changes made:
    
    Co-authored-by: Christopher Johnson <cjbeats@gmail.com>
    Co-authored-by: Amir Marcel <amirmarcel@yahoo.com>
    Co-authored-by: Corey Van Splinter <cvanspl1@gmail.com>

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Main from "./containers/Main";
 import Output from "./containers/Output";
 import { setConfigs } from "../redux/reducers/configsSlice";
@@ -10,14 +10,18 @@ function App() {
   const dispatch = useDispatch();
 
   window.addEventListener("message", (event) => {
-    console.log(event, "e<>");
-    switch (event.data.command) {
-      case "data":
-        dispatch(setRoutes(event.data.data));
+    const message = event.data;
 
+    switch (message.command) {
+      case "data":
+        if (message.data) {
+          dispatch(setRoutes(message.data));
+        } else {
+          dispatch(setRoutes({}));
+        }
         break;
       case "config":
-        dispatch(setConfigs(event.data.out));
+        dispatch(setConfigs(message.out));
         break;
       default:
     }
