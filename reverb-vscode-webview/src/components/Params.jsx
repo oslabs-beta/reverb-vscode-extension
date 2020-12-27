843e8b4133eb5cf3af96709e6af96feae2ffccd5
Sean <itsmesean@pm.me>
2020-12-26 21:44:18 -0800

    remove unused react hook form library

--- reverb-vscode-webview/src/components/Params.jsx
+++ reverb-vscode-webview/src/components/Params.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { currentUrl } from '../redux/reducers/inputStateSlice';
-import { useForm } from 'react-hook-form';
 
 import { params } from '../redux/reducers/viewContextSlice';
 
